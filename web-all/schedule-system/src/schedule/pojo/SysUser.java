470becf6d12fdfc266b5e2ad4e6e331aa3cb80a1
maozhihao <18862776370@163.com>
2024-09-25 08:38:05 +0800

    mvc模型是pojo层的

---
+++ web-all/schedule-system/src/schedule/pojo/SysUser.java
@@ -0,0 +1,90 @@
+package schedule.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/*
+1、实体类的属性名和表格的列名应该对应
+2、必须实现序列化接口
+3、应该重写累的hashcode和equals方法
+4、必须具备无参构造器
+5、必须具有getter 和setter方法
+
+
+可以使用lombok生成内容，getter和setter等一些方法
+1、需要有lombok插件
+2、检查是否勾选enable annnotation processing    setting --build,exeute--annotation process
+3、导入依赖
+
+
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data   // 相当于getter ，setter，equals，hashcode
+public class SysUser implements Serializable {
+    private String username;
+
+    private String userPwd;
+
+    private Integer uid;
+
+    public SysUser() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SysUser sysUser = (SysUser) o;
+        return Objects.equals(username, sysUser.username) && Objects.equals(userPwd, sysUser.userPwd) && Objects.equals(uid, sysUser.uid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, userPwd, uid);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    @Override
+    public String toString() {
+        return "SysUser{" +
+                "username='" + username + '\'' +
+                ", userPwd='" + userPwd + '\'' +
+                ", uid=" + uid +
+                '}';
+    }
+
+    public SysUser(String username, String userPwd, Integer uid) {
+        this.username = username;
+        this.userPwd = userPwd;
+        this.uid = uid;
+    }
+}
---
+++ web-all/schedule-system/src/schedule/test/TestLomomok.java
@@ -0,0 +1,10 @@
+package schedule.test;
+
+import schedule.pojo.SysUser;
+
+public class TestLomomok {
+    public void testAnnoation(){
+        SysUser user = new SysUser("12","12",32);
+        user.getUserPwd();
+    }
+}
