520dfef07c483c7785b84d08465f55d3c1f23b10
maozhihao <18862776370@163.com>
2024-11-06 23:18:45 +0800

    142 文本渲染20241106

---
+++ demo1-vue/src/components/插值表达式.vue
@@ -0,0 +1,38 @@
+<script setup>
+/*
+插值表达式
+*/
+ let msg = "hello vue3"
+ let getMsg=()=>{
+   return "Hello vue3 message"
+ }
+ let age = 10
+ let bee = "蜜 蜂"
+ let carts = [{name:"可乐",price:3,number:10},{name:"薯片",price:6,number:8}]
+ function compute(){
+   let count =0
+   for(let index in carts){
+     count += carts[index].price*carts[index].number
+   }
+   return count
+ }
+</script>
+
+<template>
+<div>
+<h1>{{msg}}</h1>
+msg的值是{{getMsg()}}<br>
+<!-----支持常见的运算符->
+年龄{{age}} 是否成年：{{age >18 ? "是"："否"}}
+<!-----支持对象调用----->
+{{bee.split(" ").reverse()}}<br>
+{{compute()}}
+
+  
+</div>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ demo1-vue/src/components/文本渲染命令.vue
@@ -0,0 +1,50 @@
+<script setup>
+/*
+v-text识别不了带有html格式的文本
+*/
+ let msg = "hello vue3"
+ /*
+ 支持字符串模板
+ */ 
+let haha = "haha"
+let msg2 = `hello ${haha}`
+ let getMsg=()=>{
+   return "Hello vue3 message"
+ }
+ let age = 10
+ let bee = "蜜 蜂"
+ let carts = [{name:"可乐",price:3,number:10},{name:"薯片",price:6,number:8}]
+ function compute(){
+   let count =0
+   for(let index in carts){
+     count += carts[index].price*carts[index].number
+   }
+   return count
+ }
+</script>
+
+<template>
+<div>
+<h1 v-text ="msg"></h1>
+<h1 v-text ="msg2"></h1>
+/*支持常见运算符*/ 
+<h1 v-text ="age>=18?'成年':'未成年'"></h1>
+/*支持常见api调用*/ 
+<h1 v-text ="bee.split(' ')"></h1>
+/*支持函数调用*/ 
+<h1 v-text ="getMsg()"></h1>
+
+
+
+
+
+
+
+  
+</div>
+</template>
+
+<style scoped>
+
+
+</style>
