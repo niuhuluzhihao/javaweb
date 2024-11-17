7fea5e62a59d6094cb2c816ad7d2c828835ecea1
maozhihao <18862776370@163.com>
2024-11-17 23:23:11 +0800

    152 SFC

---
+++ demo11-vue05-SFC/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import Header from  './components/Header.vue'
+  import Content from  './components/Content.vue'
+  import Navigator from  './components/Navigator.vue'
+</script>
+
+<template>
+  <div>
+    <Header class="header"></Header>
+    <Content class="navigator"></Content>
+    <Navigator class = "content"></Navigator>
+  </div>
+</template>
+
+<style scoped>
+  .header{
+    height: 80px;
+    border: 1px dolid read;
+  }
+  .navigator{
+    height: 800px;
+    border:1px solid green;
+    float:left;
+  }
+
+
+  .content{
+    width:83%;
+    height: 800px;
+    border: 1px solid blue;
+    float:right;
+  }
+</style>
---
+++ demo11-vue05-SFC/src/components/Content.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+       主要内容
+    </div>
+</template>
+
+<style scoped>
+
+</style>
