69798b8db514b0102818f9a3f873b265e344febe
maozhihao <18862776370@163.com>
2024-11-26 23:19:24 +0800

    日程第七期 178

--- demo16-pina/src/components/Operate.vue
+++ demo16-pina/src/components/Operate.vue
@@ -7,7 +7,18 @@ let person = definePerson()
 <div>
     <h1>用于修改pina数据</h1>
     <hr>
+    名字：<input type ="text" v-model="person.username">
+    年龄：<input type ="text" v-model="person.age">
+    爱好：
+        <input type="checkbox" value = "吃饭" v-model = "person.hobbies">
+        <input type="checkbox" value = "跳舞" v-model = "person.hobbies">
+        <input type="checkbox" value = "唱歌" v-model = "person.hobbies">
+    <hr>
     展示一下person的数据:{{person}}
+    <br>
+    <button @click="person.doubleAge()">年龄加倍</button>
+    <button @click="person.$reset()">恢复属性</button>
+
 </div>
 
 </template>
